4c6c15f0fa219229fede6e93c7058a9bbfdba163
GloryOfTheCPSU <mailforgitforproject@gmail.com>
2020-01-09 23:38:43 +0300

    otte

--- ottergram/js/common.js
+++ ottergram/js/common.js
@@ -1,9 +1,11 @@
 const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';//увеличенное изображение
 const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+const HIDDEN_DETAIL_CLASS = 'hidden-detail';
+const ESC_KEY = 27;
+const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+const TINY_EFFECT_CLASS = 'is-tiny';
 let firstThumbnail = document.querySelector(THUMBNAIL_LINK_SELECTOR);
-
-
 
 function setDetails(imageUrl, titleText) {
   let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
@@ -24,6 +26,7 @@ function addThumbClickHandler(thumb){
   thumb.addEventListener('click', function(event){
     event.preventDefault();
     setDetailsFromThumb(thumb);
+    showDetails();``
   })
 }
 function getThumbnailsArray(){
@@ -31,8 +34,33 @@ function getThumbnailsArray(){
   let thumbnailArray = [].slice.call(thumbnails);
   return thumbnailArray;
 }
+
+function hideDetails(){
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+function showDetails(){
+  let frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function(){
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  }, 100);
+  
+}
+
+function addKeyPressHandler(){
+    document.body.addEventListener('keyup', function(event){
+    event.preventDefault();
+    console.log(event.keyCode);
+      if(event.keyCode === ESC_KEY){
+        hideDetails();
+      }
+  });
+}
+
 function initializeEvents() {
   let thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
+  addKeyPressHandler();
 }
 initializeEvents();
